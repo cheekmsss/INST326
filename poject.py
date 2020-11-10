17c988b9cc054dacff58761e7989ac5b26232ecc
cheekmsss <cchuku@terpmail.umd.edu>
2020-11-10 13:15:47 -0500

    created init and avilable_funds module

---
+++ poject.py
@@ -0,0 +1,12 @@
+class ExpenseTracker:
+    '''
+    Program that allows users to track expenses. 
+    '''
+    def __init__(self):
+        '''
+        Initializes the user attribute and tuple that will hold data.
+        '''
+    def available_funds(self):
+        '''
+        Allows user to input available funds
+        '''                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
